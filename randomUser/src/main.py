cc1a4f748b2fae0e7c64ef8a27e636475d1b2854
NotEduardo <35389164+notEduardo@users.noreply.github.com>
2021-11-11 15:11:01 -0600

    fixed putRecord Call by adding missing parameters

--- randomUser/src/main.py
+++ randomUser/src/main.py
@@ -1,12 +1,14 @@
 import requests
 import boto3
 import json
+import uuid
 
 counter = 0
 client = boto3.client('kinesis', region_name='us-east-1')
-while(counter < 100):
+pKey = str(uuid.uuid4)
+while(counter < 10000):
     counter = counter + 1
     person = requests.get('https://randomuser.me/api/')
     data = json.dumps(person.json())
     print(data)
-    client.putRecord(data)
+    client.putRecord(StreamName='randomUser', Data=data, PartitionKey=pKey)
